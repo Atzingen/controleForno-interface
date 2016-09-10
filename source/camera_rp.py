42b5705ef996937a60873a0aa05c1f5646a49b6a
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2016-09-09 22:56:43 -0300

    mudanca no import da camera
    de forno_run para app_run

--- source/camera_rp.py
+++ source/camera_rp.py
@@ -1,7 +1,7 @@
 # -*- coding: latin-1 -*-
 from __future__ import division
-from forno_run import Ui_MainWindow
-from PyQt4 import QtGui, QtCore 
+from app_run import Ui_MainWindow
+from PyQt4 import QtGui, QtCore
 from functools import partial
 import sys, time
 try:
@@ -10,33 +10,33 @@ except:
 	pass
 
 def tira_foto(self):
-	''' Método que tira fotos com a picamera
-	A foto tirada é mostrada no label_15, que foi alterado para mostrar imagens.
-	As configurações de tempo de exposição e resolução são configuradas baseada nas 
-	informações da GUI '''
+	''' Mï¿½todo que tira fotos com a picamera
+	A foto tirada ï¿½ mostrada no label_15, que foi alterado para mostrar imagens.
+	As configuraï¿½ï¿½es de tempo de exposiï¿½ï¿½o e resoluï¿½ï¿½o sï¿½o configuradas baseada nas
+	informaï¿½ï¿½es da GUI '''
 	if sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):	# picamera apenas para raspberry (sistema unix)
 		with picamera.PiCamera() as camera:										# Objeto para capturar e manipular as imagens
-			valores = self.ui.comboBox_3.currentText().split('x')				# Pega os valores da resolução na combobox
-			img_x, img_y = int(valores[0]), int(valores[1])						
-			camera.resolution = (img_x, img_y)									# Resolução horizontal e vertical em função da combobox
-			camera.start_preview()												# Inicia a câmera
+			valores = self.ui.comboBox_3.currentText().split('x')				# Pega os valores da resoluï¿½ï¿½o na combobox
+			img_x, img_y = int(valores[0]), int(valores[1])
+			camera.resolution = (img_x, img_y)									# Resoluï¿½ï¿½o horizontal e vertical em funï¿½ï¿½o da combobox
+			camera.start_preview()												# Inicia a cï¿½mera
 			tempo_delay = int(self.ui.spinBox_2.value())/1000  					# valor do tempo para estabilizar a imagem
 			time.sleep(tempo_delay)												# tempo para estabilizar a imagem
 			camera.capture('teste.jpg')											# Captura a imagem
 			self.alerta_toolbar('tirando foto')									# debug - retirar
 	else:
-		self.alerta_toolbar('Raspicam não disponível')   						# Quando estiver testando no windows # debug - retirar	
+		self.alerta_toolbar('Raspicam nï¿½o disponï¿½vel')   						# Quando estiver testando no windows # debug - retirar
 	self.ui.label_15.setScaledContents(True)									# Escala a imagem para mostrar na GUI
 	self.ui.label_15.setPixmap(QtGui.QPixmap("teste.jpg"))						# Insere a imagem na tela
 	self.alerta_toolbar('foto salva')
 
 def foto_update(self):
-	''' Método recursivo. 
-	É chamado quando a checkbox de autoupdate da imagem é ativada.
-	A função ativa uma thread do QT no modo singleShot após a quantidad de tempo escolhida no 
-	spinBox da GUI. caso a checkbox continue ativada, a função se chamará novamente de forma recursiva
-	até que a checkbox seja desabilitada ou a conecção seja desfeita. '''
+	''' Mï¿½todo recursivo.
+	ï¿½ chamado quando a checkbox de autoupdate da imagem ï¿½ ativada.
+	A funï¿½ï¿½o ativa uma thread do QT no modo singleShot apï¿½s a quantidad de tempo escolhida no
+	spinBox da GUI. caso a checkbox continue ativada, a funï¿½ï¿½o se chamarï¿½ novamente de forma recursiva
+	atï¿½ que a checkbox seja desabilitada ou a conecï¿½ï¿½o seja desfeita. '''
 	if self.ui.checkBox_10.isChecked():											# Chama a Thread apenas se a checkbox estiver ativada
-		tira_foto(self)															# Função que tira a foto
-		tempo_delay = 1000*int(self.ui.spinBox_3.value())						# Tempo até ativar a thread para chamar a função novamente
+		tira_foto(self)															# Funï¿½ï¿½o que tira a foto
+		tempo_delay = 1000*int(self.ui.spinBox_3.value())						# Tempo atï¿½ ativar a thread para chamar a funï¿½ï¿½o novamente
 		self.timer_foto.singleShot(tempo_delay,partial(foto_update,self))		# Chama a Thread - modo singleshot
